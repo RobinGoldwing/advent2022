daf2df24b068298e2d454c5335d1ec64c5de556d
Robin <robingoldwing@gmail.com>
2022-12-08 03:10:40 +0100

    day 06

---
+++ day06/4repeat2.py
@@ -0,0 +1,18 @@
+#!/bin/env python
+
+temp = [' ',' ',' ',' ',' ',' ',' ',' ','m','m','q',' ',' ',' ']
+
+test = ()
+
+with open('input.txt') as file:
+    x = 0
+    for line in file.readlines():
+        for char in line:
+            temp.append(char)
+            temp.pop(0)
+            x += 1
+            # print(x, temp, sep = ' ')
+            test = set(temp)
+            if len(test) == len(temp):
+                print(x)
+                break
